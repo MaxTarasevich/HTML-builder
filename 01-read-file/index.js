67857b0cdc850e0e31ca1cb51fc2fb369a995b51
Max <maxtarasevich@gmail.com>
2022-05-19 10:15:22 +0300

    feat: task-1 ready

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+const fs = require('fs')
+
+const steam = new fs.ReadStream(path.resolve('text.txt'))
+
+steam.on('readable',()=>{
+    let data = steam.setEncoding('utf-8').read()
+    console.log(data)
+})
